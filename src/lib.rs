a07b07e266297f49ff29ef8f79b004651ff0f915
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-01-09 06:50:45 +0100

    docs: copy `README.md` into `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,24 @@
-//! # Resize Slice
-//!
-//! Shrink or enlarge a slice (given a larger slice) in safe Rust.
-//!
+//! [![license: MIT](https://img.shields.io/crates/l/resize_slice2)](https://github.com/benthillerkus/resize_slice/blob/main/LICENSE)
+//! [![build status docs](https://img.shields.io/docsrs/resize_slice2)](https://docs.rs/resize_slice2)
+//! [![crate version](https://img.shields.io/crates/v/resize_slice2)](https://crates.io/crates/resize_slice2)
+//! 
+//! # resize slice (2)
+//! 
+//! Enlarge and shrink slices (given a larger slice) in safe Rust.
+//! 
+//! Not to be confused with [resize-slice](https://crates.io/crates/resize_slice), which can only shrink slices and uses `unsafe` 👻
+//! 
+//! # Example
+//! 
 //! ```rust
 //! use resize_slice2::ResizeSlice;
-//!
 //! let source = &["a", "b", "c", "d", "e", "f"];
 //! let slice = &source[1..4];
 //! assert_eq!(slice, &["b", "c", "d"]);
 //! let resized = slice.try_resize(source, 0..1).unwrap();
 //! assert_eq!(resized, &["b", "c", "d", "e"]);
 //! ```
-//!
+//! 
 //! So a range of `1..-1` would move the start one to the right and move the end one to the left.
 //! ```blank
 //! source: |------------------------|
